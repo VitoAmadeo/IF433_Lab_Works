8465357c85c077c5e86e7f5ba56fd1d870cc22eb
Vito <vito.amadeo@student.umn.ac.id>
2026-09-04 15:49:12 +0700

    Week01: (Tugas2) implement discount logic

--- src/oop_001_VitoAmadeoD/week01/GameStore.kt
+++ src/oop_001_VitoAmadeoD/week01/GameStore.kt
@@ -1,8 +1,12 @@
 package oop_001_VitoAmadeoD.week01
 
+fun calcPrice(price: Int) = if(price > 500000) (price * 0.8) else (price * 0.9)
+
 fun main()
 {
-    val gameTitle: String? = null
-    val price: Int?= null
+    val gameTitle= "Red Dead Redemption 3"
+    val price= 650000
 
+    calcPrice(price)
+    println("Price: $price")
 }
