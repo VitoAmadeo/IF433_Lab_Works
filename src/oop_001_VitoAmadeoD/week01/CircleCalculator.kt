0e759bf3ff0f28203dffa431982409324b05888c
Vito <vito.amadeo@student.umn.ac.id>
2026-09-04 14:45:23 +0700

    Week01: (Tugas1) convert checkSize to expression body function

--- src/oop_001_VitoAmadeoD/week01/CircleCalculator.kt
+++ src/oop_001_VitoAmadeoD/week01/CircleCalculator.kt
@@ -12,15 +12,4 @@ fun main(args: Array<String>)
     checkSize(area)
 }
 
-fun checkSize(area: Double)
-{
-    if (area > 100)
-    {
-        println("This is a Big Circle")
-    }
-    else
-    {
-        println("This is a Small Circle")
-
-    }
-}
+fun checkSize(area: Double) = if (area >100) "This is a Big Circle" else "This is a Small Circle"
