e12f4d39e5609e09e257e506c9ca797499e70168
Vito <vito.amadeo@student.umn.ac.id>
2026-09-04 15:55:20 +0700

    Week01: (Tugas2) implement Receipt Printing

--- src/oop_001_VitoAmadeoD/week01/GameStore.kt
+++ src/oop_001_VitoAmadeoD/week01/GameStore.kt
@@ -1,12 +1,14 @@
 package oop_001_VitoAmadeoD.week01
 
-fun calcPrice(price: Int) = if(price > 500000) (price * 0.8) else (price * 0.9)
+fun calcPrice(price: Int): Int = if(price > 500000) (price * 80)/100 else (price * 90)/100
+
+fun receipt(gameTitle: String, finalPrice: Int) = println("Title: $gameTitle, Price: $finalPrice")
 
 fun main()
 {
     val gameTitle= "Red Dead Redemption 3"
     val price= 650000
 
-    calcPrice(price)
-    println("Price: $price")
+    val finalPrice= calcPrice(price)
+    receipt(gameTitle, finalPrice)
 }
