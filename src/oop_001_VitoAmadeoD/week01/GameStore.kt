f730acc24d42cb3a46df2f6641f1416331409db3
Vito <vito.amadeo@student.umn.ac.id>
2026-09-04 16:00:39 +0700

    Week01: (Tugas2) implement null safety for user note

--- src/oop_001_VitoAmadeoD/week01/GameStore.kt
+++ src/oop_001_VitoAmadeoD/week01/GameStore.kt
@@ -2,7 +2,13 @@ package oop_001_VitoAmadeoD.week01
 
 fun calcPrice(price: Int): Int = if(price > 500000) (price * 80)/100 else (price * 90)/100
 
-fun receipt(gameTitle: String, finalPrice: Int) = println("Title: $gameTitle, Price: $finalPrice")
+fun receipt(gameTitle: String?, finalPrice: Int?)
+{
+    val safeTitle = gameTitle ?: "No data / notes"
+    val safePrice = finalPrice ?: "No data / notes"
+    println("Title: $safeTitle, Price: $safePrice")
+}
+
 
 fun main()
 {
