3637fd42c38e957ee5bd1e1920a4d5259eac3774
Vito <vito.amadeo@student.umn.ac.id>
2026-09-11 20:16:36 +0700

    Week02: (Task1) implement library input system

--- src/oop_001_VitoAmadeoD/week02/Loan.kt
+++ src/oop_001_VitoAmadeoD/week02/Loan.kt
@@ -1,16 +1,28 @@
 package oop_001_VitoAmadeoD.week02
 
-class loan(val bookTitle: String,val borrower: String,var loanDuration: Int = 0)
+import java.util.Scanner
+
+class Loan(val bookTitle: String,val borrower: String,var loanDuration: Int = 0)
 {
-    fun calculateFine()
-    {
-        if (loanDuration > 3)
-        {
-            val fine: Int= (loanDuration -3) * 2000
-        }
-        else
-        {
-            val fine: Int = 0
-        }
-    }
+    fun calculateFine() = if (loanDuration > 3) (loanDuration - 3)*2000 else 0
+}
+
+fun main()
+{
+    val scanner = Scanner(System.`in`)
+    print("Insert Title : ")
+    val bookTitle = scanner.nextLine()
+
+    print("Insert Name : ")
+    val borrower = scanner.nextLine()
+
+    print("Insert how many days you have borrowed : ")
+    val localDuration = scanner.nextInt()
+
+    scanner.nextLine()
+
+    val loan = Loan(bookTitle, borrower, localDuration)
+    val fine = loan.calculateFine()
+    println("Your book: ${loan.bookTitle}, your name: ${loan.borrower},Your Duration: ${loan.loanDuration}, fine: $fine")
+
 }
