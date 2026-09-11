957ff6a2923b2052bef13d91ab109d24b738416f
Vito <vito.amadeo@student.umn.ac.id>
2026-09-11 21:47:09 +0700

    Week02: (Task2) Implement the Game and Random Critical Hits

--- src/oop_001_VitoAmadeoD/week02/Hero.kt
+++ src/oop_001_VitoAmadeoD/week02/Hero.kt
@@ -2,7 +2,7 @@ package oop_001_VitoAmadeoD.week02
 
 import java.util.Scanner
 
-class hero(val name: String, var hp: Int= 100, val baseDamage: Int)
+class Hero(val name: String, var hp: Int= 100, val baseDamage: Int)
 {
     fun attack(targetName: String) = println("$name has attacked $targetName!")
     fun takeDamage(damage: Int)
@@ -14,4 +14,54 @@ class hero(val name: String, var hp: Int= 100, val baseDamage: Int)
             }
         }
     fun isAlive() = hp>0
+}
+
+fun main()
+{
+    val scanner = Scanner(System.`in`)
+    print("Hero's name: ")
+    val name = scanner.nextLine()
+    print("Damage Output: ")
+    val baseDamage = scanner.nextInt()
+    scanner.nextLine()
+    val hero = Hero(name, 100, baseDamage)
+    var enemyHp = 100
+
+
+    while(hero.isAlive() && enemyHp >0)
+    {
+        println("Your HP = ${hero.hp}, Enemy's HP = $enemyHp")
+        println("Menu")
+        println("1. Fight")
+        println("2. Run")
+        val opt = scanner.nextInt()
+
+        if(opt == 1)
+        {
+            enemyHp -= hero.baseDamage*((1..2).random()) // random crit
+            hero.attack("Enemy")
+            if(enemyHp > 0)
+            {
+                hero.takeDamage((10..20).random()*(1..2).random()) // enemy random crit
+            }
+        }
+        else if(opt == 2)
+        {
+            println("You Ran!")
+            break
+        }
+
+    }
+    if(!hero.isAlive()) // dead
+    {
+        println("You Lose!, Enemy's HP : $enemyHp")
+    }
+    else if(hero.isAlive() && enemyHp>0) // run
+    {
+        println("Your HP : ${hero.hp}, Enemy's HP : $enemyHp")
+    }
+    else // win
+    {
+        println("You Win!, Your HP :${hero.hp}")
+    }
 }
