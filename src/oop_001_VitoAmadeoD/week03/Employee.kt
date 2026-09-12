d6de5e061e905750bb10070d1dd6e733c7f6b57b
Vito <vito.amadeo@student.umn.ac.id>
2026-09-12 10:33:44 +0700

    Week03: Test all encapsulation logic in main and Cleanup Code

--- src/oop_001_VitoAmadeoD/week03/Employee.kt
+++ src/oop_001_VitoAmadeoD/week03/Employee.kt
@@ -28,13 +28,16 @@ class Employee(val name: String)
     {
         println("Karyawan: $name, Rating $performanceRating")
     }
+    val tax: Double
+        get() = salary * 0.1
 }
 
 fun main()
 {
     val e = Employee("Budi")
-    e.salary = 5000
-    e.printStatus()
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
     e.increasePerformance()
-    e.printStatus()
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
