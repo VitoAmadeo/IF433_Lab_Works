c01a514f573915a4d87b0f5c0056f2624eee0ce7
Vito <vito.amadeo@student.umn.ac.id>
2026-09-19 11:24:46 +0700

    Week04: (task2) test Employee hierarchy in main

--- src/oop_001_VitoAmadeoD/week4/Main2.kt
+++ src/oop_001_VitoAmadeoD/week4/Main2.kt
@@ -2,7 +2,16 @@ package oop_001_VitoAmadeoD.week4
 
 fun main()
 {
-    val emp = Employee("Joe", 500000)
+    val emp = Employee("Joe", 500000, 0)
     emp.work()
+    emp.calculateBonus()
+
+    val dvp = Developer("Joe mamas", 500000, 0, "kotlin")
+    dvp.work()
+    dvp.calculateBonus()
+
+    val mng = Manager("Joe mama", 500000, 0)
+    mng.work()
+    mng.calculateBonus()
 
 }
